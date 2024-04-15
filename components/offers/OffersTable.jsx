aeab1834ce31df1fcf6d034dcca1cdefb9b433dc
patrik stojsavljevic <patrik.stojsavljevic@elatus.net>
2024-04-15 11:49:05 +0200

    .

--- components/offers/OffersTable.jsx
+++ components/offers/OffersTable.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
   TableEmptyRows,
   TableHeadCustom,
@@ -11,6 +11,8 @@ import OffersTableRow from "./OffersTableRow";
 import { useDeleteOffer, useGetOffers } from "@/app/actions/GetOffers";
 import Link from "next/link";
 import Image from "next/image";
+import useDebounce from "@/app/hooks/useDebounce";
+import clsx from "clsx";
 
 const TABLE_HEAD = [
   { id: "id", label: "ID", align: "left" },
@@ -24,10 +26,53 @@ const TABLE_HEAD = [
 
 export default function OffersTable({ limit }) {
   const { push } = useRouter();
+  const [offersList, setOffersList] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [filterName, setFilterName] = useState("");
+  const debouncedSearchValue = useDebounce(searchValue, 500);
 
   const { data: offers, isLoading } = useGetOffers();
-
   const { mutate: deleteOffer } = useDeleteOffer();
+
+  useEffect(() => {
+    if (searchValue.length > 3) {
+      setFilterName(debouncedSearchValue);
+    } else {
+      setFilterName("");
+    }
+  }, [debouncedSearchValue, filterName]);
+
+  useEffect(() => {
+    if(!isLoading){
+      setOffersList(offers)
+    }
+  }, [offers]);
+
+
+  const handleSearchFilterChange = (event) => {
+    setSearchValue(event.target.value);
+    handleFilterList(event.target.value)
+  };
+
+  const handleFilterList = (value) => {
+    if(offers){
+      const exactMatch = offers.find(offer => offer?.customer_name.toLowerCase() === value.toLowerCase()),
+          partialMatches = offers.filter(offer => offer?.customer_name.toLowerCase().includes(value.toLowerCase())),
+          vagueMatches = offers.filter(offer =>
+              value
+                  .toLowerCase()
+                  .split(' ')
+                  .every(w => offer?.customer_name.toLowerCase().includes(w))
+          )
+      const tmpList = [...new Set([exactMatch, ...partialMatches, ...vagueMatches])]
+      const newList = tmpList.filter(offer => {
+        if(offer !== undefined) {
+          return offer
+        }
+      });
+      setOffersList(newList);
+    }
+  }
 
   const handleDeleteRow = (id) => {
     deleteOffer(id);
@@ -43,16 +88,47 @@ export default function OffersTable({ limit }) {
 
   return (
     <div className="card">
-      <div className="p-4">
+      <div className="p-4 flex items-center gap-5">
         <h2 className="text-[18px] font-bold">Offers list</h2>
+        <div className="relative">
+          <input
+              type="text"
+              name="search"
+              value={searchValue}
+              placeholder="Search..."
+              onChange={handleSearchFilterChange}
+              onKeyUp={handleSearchFilterChange}
+              className={clsx(
+                  `
+              block 
+              w-full 
+              rounded-[5px] 
+              py-[5px]
+              px-[12px]
+              sm:py-[7px]
+              sm:px-[15px]
+              text-black
+              border-1
+              border
+              bg-primary-lightred 
+              border-primary-gray 
+              placeholder:text-gray-400 
+              focus:bg-white
+              focus:outline-none
+              focus:ring-transparent
+              text-sm
+              sm:text-md 
+            `,
+              )}
+          />
+        </div>
       </div>
       <TableResponsiveWrap>
         <table className="min-w-[800px]">
-          <TableHeadCustom headLabel={TABLE_HEAD} rowCount={offers?.length} />
-
+          <TableHeadCustom headLabel={TABLE_HEAD} rowCount={offersList?.length} />
           <tbody>
             {!isLoading && !limit
-              ? offers?.map((row) => (
+              ? offersList?.map((row) => (
                   <OffersTableRow
                     key={row.id}
                     row={row}
@@ -61,7 +137,7 @@ export default function OffersTable({ limit }) {
                     onViewRow={() => handleViewRow(row.id)}
                   />
                 ))
-              : offers
+              : offersList
                   ?.slice(0, limit)
                   ?.map((row) => (
                     <OffersTableRow
@@ -74,12 +150,12 @@ export default function OffersTable({ limit }) {
                   ))}
             <TableEmptyRows
               emptyRows={
-                !isLoading && offers?.lenght > 0 ? 5 - offers?.length : 0
+                !isLoading && offersList?.lenght > 0 ? 5 - offersList?.length : 0
               }
-              height={!isLoading && offers?.length < 5 ? 60 : 0}
+              height={!isLoading && offersList?.length < 5 ? 60 : 0}
             />
             <TableNoData
-              isNotFound={offers?.length < 1 || offers === undefined}
+              isNotFound={offersList?.length < 1 || offersList === undefined}
               title={isLoading ? "Table is loading..." : "No data in table"}
             />
           </tbody>
